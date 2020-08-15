97ab9d27a9e278fa898d4540da6a6607c7f54911
kAUdo <kaudo@msn.com>
2020-08-15 23:42:42 +0900

    HPC19

--- app_kpopmv/languages.js
+++ app_kpopmv/languages.js
@@ -1,43 +1,38 @@
 $.lang={};
 $.lang.en={
 	lang:'en',
-    title:'Junnam Baby Care',
-    child:'Child',
-    room:'Room',
-    over:'Over',
-	less:'Less',
-	button_map_naver:'VIEW ON<br/>NaverMap',
-	button_map_kakao:'VIEW ON<br/>KakaoMap',
+  title:'kpop music video',
+  broadcast:'broadcast',
+  agency:'agency',
+  celebrity:'celebrity',
+	youtuber:'youtuber',
+	viewall:'view all',
 };
 $.lang.ko={
 	lang:'ko',
-    title:'전라남도 어린이집',
-    child:'정원',
-    room:'교실수',
-    over:'이상',
-	less:'이하',
-	button_map_naver:'네이버지도<br/>에서보기',
-	button_map_kakao:'카카오맵<br/>에서보기',
+	title:'kpop music video',
+  broadcast:'broadcast',
+  agency:'agency',
+  celebrity:'celebrity',
+	youtuber:'youtuber',
+	viewall:'view all',
 };
-$.lang.jaxx={
+$.lang.ja={
 	lang:'ja',
-    title:'Junnam Baby Care',
-    child:'Child',
-    room:'Room',
-    over:'Over',
-	less:'Less',
-	button_map_naver:'VIEW ON<br/>NaverMap',
-	button_map_kakao:'VIEW ON<br/>KakaoMap',
+	title:'kpop music video',
+  broadcast:'broadcast',
+  agency:'agency',
+  celebrity:'celebrity',
+	youtuber:'youtuber',
+	viewall:'view all',
 };
-$.lang.zhxx={
-	lang:'zh',
-    title:'Junnam Baby Care',
-    child:'Child',
-    room:'Room',
-    over:'Over',
-	less:'Less',
-	button_map_naver:'VIEW ON<br/>NaverMap',
-	button_map_kakao:'VIEW ON<br/>KakaoMap',
+$.lang.zh={
+	title:'kpop music video',
+  broadcast:'broadcast',
+  agency:'agency',
+  celebrity:'celebrity',
+	youtuber:'youtuber',
+	viewall:'view all',
 };
 
 if(location.href.indexOf('lang=')<0) $.lang=$.lang.en;
