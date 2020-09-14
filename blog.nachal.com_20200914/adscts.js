0b1e8f78762c6956320c46cef5dea80de4214e08
AHNDOORI <kaudo@msn.com>
2020-09-14 22:16:14 +0900

    HMP17

---
+++ blog.nachal.com_20200914/adscts.js
@@ -0,0 +1,5 @@
+jQuery('.blogview_content .imageblock').each(function(key,value){
+	jQuery(this).append('<br/><ins class="adsbygoogle" style="display:inline-block;width:320px;height:50px" data-ad-client="ca-pub-5585342941485872" data-ad-slot="5137419013"></ins>');
+	(adsbygoogle = window.adsbygoogle || []).push({});
+});
+// <ins class="adsbygoogle" style="display:inline-block;width:320px;height:100px" data-ad-client="ca-pub-5585342941485872" data-ad-slot="4164079750"></ins>
---
+++ blog.nachal.com_20200914/adsense.js
@@ -0,0 +1,5 @@
+google_ad_client = "ca-pub-5585342941485872";
+/* 728x90 애드센스 이미지 배너 */
+google_ad_slot = "9373964504";
+google_ad_width = 728;
+google_ad_height = 90;
